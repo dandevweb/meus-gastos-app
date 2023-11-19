f4fa4398f4edada090aecaf88cda41c85390b56f
dandevweb <dandevweb@gmail.com>
2023-11-19 11:52:33 -0300

    feat: database structure and models

---
+++ app/Models/UserPlan.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserPlan extends Model
+{
+    use HasFactory;
+
+    protected $table = 'user_plan';
+
+    protected $guarded = ['id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function plan(): BelongsTo
+    {
+        return $this->belongsTo(Plan::class);
+    }
+}
